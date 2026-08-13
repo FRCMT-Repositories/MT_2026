4180ba002aac362b29379cb333fcaf1f7b2a1cff
FRCMT <riajd2011@gmail.com>
2026-08-13 13:18:35 -0400

    Adicionado Tank Drive
    -Atualização de CAD dos subsistemas para montagem no tank
    -Novos arquivos para import tank no advantage scope
    -Novo código prevendo movimentação tank (apenas simulação)

--- Code/DifferentialDrive/src/main/java/frc/robot/RobotContainer.java
+++ Code/DifferentialDrive/src/main/java/frc/robot/RobotContainer.java
@@ -10,12 +10,19 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
+import frc.robot.subsystems.SimElevator;
+import frc.robot.subsystems.SimHand;
+import frc.robot.subsystems.SimIntake;
 
 public class RobotContainer {
 
   private final TankSubsystem tank = new TankSubsystem();
 
   private final CommandXboxController driver =new CommandXboxController(0);
+
+  private SimIntake mSimIntake = new SimIntake(20, 1.5, 3);
+  private SimElevator mSimElevator = new SimElevator(400, 1.5);
+  private SimHand mSimHand = new SimHand(-180, 180, 1.5, 3);
 
   public RobotContainer() {
     configureBindings();
@@ -30,6 +37,28 @@ public class RobotContainer {
 
   private void configureBindings() {
     driver.start().onTrue(tank.runOnce(() -> tank.resetPose(new Pose2d(2.0, 4.0, new Rotation2d()))));
+
+    driver.a().onTrue(Commands.runOnce(() -> mSimIntake.setPosition(20, 3)));
+    driver.b().onTrue(Commands.runOnce(() -> mSimIntake.setPosition(0, 3)));
+
+    driver.a().onTrue(Commands.runOnce(() -> mSimIntake.setVelocity(1)));
+    driver.b().onTrue(Commands.runOnce(() -> mSimIntake.setVelocity(0)));
+
+    driver.povUp().onTrue(mSimElevator.CMDsetPosition(400, 3));
+    driver.povDown().onTrue(mSimElevator.CMDsetPosition(0, 3));
+
+    driver.povLeft().onTrue(Commands.runOnce(() -> mSimHand.setAngle(-45, 3)));
+    driver.povRight().onTrue(Commands.runOnce(() -> mSimHand.setAngle(45, 3)));
+
+    driver.y().onTrue(Commands.runOnce(() -> mSimHand.setVelocity(2)));
+    driver.x().onTrue(Commands.runOnce(() -> mSimHand.setVelocity(-2)));
+    driver.b().onTrue(Commands.runOnce(() -> mSimHand.setVelocity(0)));
+
+    driver.back().onTrue(Commands.runOnce(() -> mSimIntake.setPosition(0, 3)));
+    driver.back().onTrue(Commands.runOnce(() -> mSimIntake.setVelocity(0)));
+    driver.back().onTrue(Commands.runOnce(() -> mSimElevator.setPosition(0, 3)));
+    driver.back().onTrue(Commands.runOnce(() -> mSimHand.setAngle(0, 3)));
+    driver.back().onTrue(Commands.runOnce(() -> mSimHand.setVelocity(0)));
   }
 
   public Command getAutonomousCommand() {
---
+++ Code/DifferentialDrive/src/main/java/frc/robot/subsystems/SimIntake.java
@@ -0,0 +1,128 @@
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SimIntake extends SubsystemBase {
+    private PIDController intakePID = new PIDController(1.5, 0, 0.0);
+    private PIDController intakeVelocity = new PIDController(1.5, 0, 0.0);
+    
+    private static double Encoder_Zero = 0;
+    private static double Encoder_Foward = 50;
+    
+    private final static double Intake_Zero = 0;
+    private final static double Intake_Foward = 0.214334;
+    private static double SetPosition = 0;
+    private static double CurrentPosition = 0;
+    private static double OutPosition = 0;
+
+    private static double SetpointVelocity = 0;
+    private static double CurrentVelocity = 0;
+    private static double Rotation = 0;
+
+    public SimIntake(double realFoward, double kPFoward, double kPWheel){
+        Encoder_Foward = realFoward;
+        intakePID.setP(kPFoward);
+        intakeVelocity.setP(kPWheel);
+    }
+
+    @Override
+    public void periodic() {
+
+        intakeMove();
+        intakeVelocity();
+
+        double Intake_CO = CurrentPosition * Math.sin(Math.toRadians(5.71));
+        double Intake_CA = CurrentPosition * Math.cos(Math.toRadians(5.71));
+
+        Logger.recordOutput("SubSystemSim/Intake/Velocity", CurrentVelocity);
+
+        Logger.recordOutput("SubSystemSim/Intake/Gaveta", new Pose3d[] { new Pose3d(
+            Intake_CA, 0.0, -Intake_CO,new Rotation3d(0.0, 0, 0))});
+
+        Logger.recordOutput("SubSystemSim/Intake/Coletor", new Pose3d[] { new Pose3d(
+            Intake_CA + 0.336804 , 0.0, -Intake_CO + 0.238576 , new Rotation3d(0, Rotation, 0))});
+    }
+
+    /**
+    * @return um valor linear dentro do range estabelecido.
+    *
+    * @param x Variavel de leitura.
+    * @param in_min Valor minimo de entrada da variavel x.
+    * @param in_max Valor maximo de entrada da variavel x.
+    * @param out_min Valor de saida minimo permitido (PODE TER B.Ozinhos).
+    * @param out_max Valor de saida maximo permitido (PODE TER B.Ozinhos).
+    */
+    public static double map(double x, double in_min, double in_max, double out_min, double out_max) {
+        double result = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        return result;
+    }
+
+    /**
+    * @return null
+    *
+    * @param kP Define o ganho proporcional do sistema Intake.
+    */
+    public void config(double kP) {
+        intakePID.setP(kP);
+    }
+
+    /**
+    * @return Retorna a posição atual de articulação do intake com base no CAD.
+    *
+    * @param null.
+    */
+    public static double getRawPosition() {
+        return CurrentPosition;
+    }
+
+    /**
+    * @return Retorna a posição atual de articulação do intake com base no encoder
+    *
+    * @param null.
+    */
+    public static double getRealPosition() {
+        return map(CurrentPosition, Intake_Zero, Intake_Foward, Encoder_Zero, Encoder_Foward);
+    }
+
+    /**
+    * @return null.
+    *
+    * @param position Define a velocidade do sistema de coleta do intake.
+    * @param kP Define o ganho proporcional do sistema de articulação do intake.
+    */
+    public void setPosition(double position, double kP) {
+        config(kP);
+        SetPosition = map(position, Encoder_Zero, Encoder_Foward, Intake_Zero, Intake_Foward);
+    }
+
+    /**
+    * @return null.
+    *
+    * @param speed Define a velocidade do sistema de coleta do intake.
+    */
+    public void setVelocity(double speed) {
+        SetpointVelocity = speed;
+    }
+
+    private void intakeMove(){
+        OutPosition = intakePID.calculate(CurrentPosition, SetPosition);
+        CurrentPosition += OutPosition * 0.02;
+        CurrentPosition = MathUtil.clamp(CurrentPosition, Intake_Zero, Intake_Foward);
+    }
+
+    private void intakeVelocity(){
+        double intakeVelocityOutput = intakeVelocity.calculate(CurrentVelocity, SetpointVelocity);
+        CurrentVelocity += intakeVelocityOutput * 0.02;
+
+        Rotation = Rotation + (0.1 * CurrentVelocity);
+        if(Rotation > Math.PI || Rotation < -Math.PI){
+            Rotation = 0;
+        }
+    }
+}
